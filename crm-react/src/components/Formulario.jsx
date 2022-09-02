21422d5f93b9630b5859b5ed80e6b726cb0c8eb6
Jesus Ortiz Alvarez <jortiz@hplaya.local>
2022-09-02 09:35:47 +0200

    video 167

--- crm-react/src/components/Formulario.jsx
+++ crm-react/src/components/Formulario.jsx
@@ -1,20 +1,30 @@
 import React from 'react'
 import {Formik, Form, Field, ErrorMessage} from 'formik'
-import * as yup from 'yup'
+import * as Yup from 'yup'
 
 const Formulario = () => {
 
-    const nuevoClienteSchema = yup.object().shape({
-        nombre: yup.string().required('El nombre del Cliente es Obligatorio'),
-        empresa: '',
-        email: '',
-        telefono: '',
-        notas: ''
+    const nuevoClienteSchema = Yup.object().shape({
+        nombre: Yup.string()
+                    .min(3, 'El nombre es muy corto')
+                    .max(20, 'El nombre es muy largo')
+                    .required('El nombre del Cliente es Obligatorio'),
+        empresa: Yup.string()
+                    .required('El nombre de la empresa es obligatorio'),
+        email:  Yup.string()
+                   .email('Email no valido')
+                   .required('El Email es obligatorio'),
+        telefono: Yup.number()
+                     .positive('Número no válido')
+                     .integer('Número no válido')
+                     .typeError('El Número no es válido')
+       
     })
 
-    const handleSubmit = () => {
-
+    const handleSubmit = (valores) => {
+        console.log(valores)
     }
+
   return (
     <div className='bg-white mt-10 px-5 py-10 rounded-md shadow-md md:w-3/4 mx-auto'>
         <h1 className='text-gray-600 font-bold text-xl uppercase text-center'> Agregar Cliente</h1>
@@ -71,6 +81,11 @@ const Formulario = () => {
                         placeholder = "Empresa del Cliente"
                         name = 'empresa'
                     />
+                    {errors.empresa && touched.empresa ? (
+                        <div className='text-center'>
+                            {errors.empresa}
+                        </div>
+                    ) : null} 
                 </div>
 
                 <div>
@@ -86,6 +101,11 @@ const Formulario = () => {
                         placeholder = "Email del Cliente"
                         name = 'email'
                     />
+                    {errors.email && touched.email ? (
+                        <div className='text-center'>
+                            {errors.email}
+                        </div>
+                    ) : null} 
                 </div>
 
                 <div>
@@ -101,6 +121,11 @@ const Formulario = () => {
                         placeholder = "Telefono del Cliente"
                         name = 'telefono'
                     />
+                    {errors.telefono && touched.telefono ? (
+                        <div className='text-center'>
+                            {errors.telefono}
+                        </div>
+                    ) : null} 
                 </div>
 
                 <div>
@@ -116,17 +141,15 @@ const Formulario = () => {
                         className = 'mt-2 block w-full p-3 bg-gray-50 h-40'
                         placeholder = "Notas del Cliente"
                         name = 'notas'
-                    />
+                    /> 
                 </div>
 
                 <input
                     type='submit'
                     value = "Agregar Cliente"
                     className='mt-5 w-full bg-blue-800 p-3 text-white uppercase font-bold text-lg'
-                />
-
-                            
-            </Form>
+                /> 
+                </Form>
              )}}
         </Formik>
     </div>
